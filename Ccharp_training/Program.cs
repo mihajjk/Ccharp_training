5a848e0ca3004d38346f2570eb5f8279b7806645
Savex24 <G.YmeOnner@yandex.ru>
2020-07-08 08:17:54 +0300

    Добавьте файлы проекта.

---
+++ Ccharp_training/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ccharp_training
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Hello world!");
+
+            Console.ReadKey();
+        }
+    }
+}
