88c8e57efdd4e9e6cc45649abb22ad5ac3a88970
Savex24 <G.YmeOnner@yandex.ru>
2020-10-18 11:48:29 +0300

    Update Program.cs

--- Ccharp_training/Program.cs
+++ Ccharp_training/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 
 namespace Ccharp_training
 {
     class Program
     {
+        static void printMass(int[] mas)
+        {
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i].ToString()+"  ");
+
+            } Console.WriteLine();
+        }
+
         static void Main()
         {
-            Console.Write("Hello world!");
+            int[] mas = new int[] {8,4,7,1,2};
+
+            printMass(mas);
+            for (int j = 1; j < mas.Length; j++)
+            {
+                for (int i = 0; i < mas.Length-j; i++)
+                {
+                    if (mas[i] > mas[i+1])
+                    {
+                        int tmp = mas[i];
+                        mas[i] = mas[i + 1];
+                        mas[i + 1] = tmp;
+                    }
+                }
+            }
+
+
+            printMass(mas);
 
             Console.ReadKey();
         }
